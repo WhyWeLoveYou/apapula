f1a2217ccd4f794e2134383777cb7affea49d63d
WhyWeLoveYou <alifventura613@gmail.com>
2024-10-02 09:45:11 +0700

    up

--- Apanjir/akumalasabsen.py
+++ Apanjir/akumalasabsen.py
@@ -1,6 +1,10 @@
 import requests 
 from requests.auth import HTTPBasicAuth 
   
-x = requests.get('https://siswa.smkn2solo.online/pages/auth/')
-  
+url = 'https://siswa.smkn2solo.online/pages/auth/checkLogin.php'
+loginnya = {
+    'username': '0063500607@smkn2-solo.net',
+    'password': 'ALIFM989'
+}
+x = requests.post(url, data=loginnya)
 print(x.text)
